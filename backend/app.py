e40b40fb07e7fadeb31119b0100be762fd76aa2e
mihhwi <mihhwi04@gmail.com>
2025-04-04 12:08:11 +0700

    Module hóa đơn và bán hàng

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from database import db
 from flask_mysqldb import MySQL
-from routes.products import products_bp  # Import routes sản phẩm
-
+from routes.products import products_bp
+from routes.invoicesRoute import invoices_bp
 app = Flask(__name__)
 CORS(app)
 
 # Cấu hình database
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "pos_system"  
+app.config['MYSQL_PASSWORD'] = "123456"
+app.config['MYSQL_DB'] = "pos_system"
 
 # Khởi tạo database
-db = MySQL(app)  
+db = MySQL(app)
 
 # Đăng ký Blueprint
 app.register_blueprint(products_bp, url_prefix='/products')
+app.register_blueprint(invoices_bp, url_prefix='/invoices')
+
+@app.route("/")
+def home():
+    return render_template("app.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
