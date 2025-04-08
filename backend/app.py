44c50f665bb86b16c7bfc4661b65a27332742dac
hoanqphann <hoanqphann@gmail.com>
2025-04-08 18:58:14 +0700

    customer

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,14 @@ from database import db
 from flask_mysqldb import MySQL
 from routes.products import products_bp
 from routes.invoicesRoute import invoices_bp
+from routes.Customer import customers_bp
 app = Flask(__name__)
 CORS(app)
 
 # Cấu hình database
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "123456"
+app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "pos_system"
 
 # Khởi tạo database
@@ -19,6 +20,7 @@ db = MySQL(app)
 # Đăng ký Blueprint
 app.register_blueprint(products_bp, url_prefix='/products')
 app.register_blueprint(invoices_bp, url_prefix='/invoices')
+app.register_blueprint(customers_bp, url_prefix='/customers')
 
 @app.route("/")
 def home():
