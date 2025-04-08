ed20ad074b7fc757ce9e8c89895e829ab35f3507
mihhwi <mihhwi04@gmail.com>
2025-04-08 19:12:19 +0700

    Backup

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ CORS(app)
 # Cấu hình database
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_PASSWORD'] = "123456"
 app.config['MYSQL_DB'] = "pos_system"
 
 # Khởi tạo database
