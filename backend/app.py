8ce8d65ad69923db4710bdc4ec4e38889cad84b8
mihhwi <mihhwi04@gmail.com>
2025-04-13 12:47:32 +0700

    update

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ CORS(app)
 # Cấu hình database
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_PASSWORD'] = "123456"
 app.config['MYSQL_DB'] = "pos_system"
 
 db = MySQL(app)
